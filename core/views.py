857b7ea732ff312cbbc9097f3429e20920690d75
kmnkit <tokyobuy2020@gmail.com>
2020-12-20 22:00:17 +0900

    home

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect, render
+from posts.models import Post
 
 
 def index(request):
-    return render(request, "home.html")
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        return render(request, "posts/post_list.html", {"posts": posts})
+    else:
+        return render(request, "home.html")
