5893a70ad53fbfeb81774d7fd063c3f1d67359b5
kmnkit <tokyobuy2020@gmail.com>
2020-12-20 22:47:30 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["westa-env.eba-meu6psda.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition
@@ -80,13 +80,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": "westa.clidrfbnacdl.ap-northeast-2.rds.amazonaws.com",
+            "NAME": "westa",
+            "USER": "westa_admin",
+            "PASSWORD": "kang7944",
+            "PORT": "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
